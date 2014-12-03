32110477ad52182a5bee7a02efbaa45faa3ce61b
Friedger Müffke <friedger@gmail.com>
2014-12-03 02:05:03 +0100

    fixes #15

--- libraries/Donations/src/main/java/org/sufficientlysecure/donations/google/util/IabHelper.java
+++ libraries/Donations/src/main/java/org/sufficientlysecure/donations/google/util/IabHelper.java
@@ -264,6 +264,7 @@ public class IabHelper {
         };
 
         Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
         if (!mContext.getPackageManager().queryIntentServices(serviceIntent, 0).isEmpty()) {
             // service available to handle that Intent
             mContext.bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
